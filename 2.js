9b629623e80d026fc9d7e2c02f7b21c13c1b9dba
vinhphan <vinhphan.dev@gmail.com>
2018-08-08 10:38:01 +0700

    sua comment

--- 2.js
+++ 2.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-		
 		$('.xuong,.kn').click(function(){
 			$('body,html').animate({scrollTop:$('#khoiedu').offset().top},700);
 		});
+		$('.gt').click(function(){
+			$('body,html').animate({scrollTop:$('.khoitt1').offset().top},700);
+			return false;
+		});
+		$('.cm').click(function(){
+			$('body,html').animate({scrollTop:$('.khoicomment').offset().top},700);
+			return false;
+		});
+		
 });
